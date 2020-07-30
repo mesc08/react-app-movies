c76d74c05383ae219596dcdcbb7437bd172a02ab
SRIJAN CHAKRABORTY <srijanch0810@gmail.com>
2020-07-30 18:24:37 +0530

    add Route/about.css and Route/about.js

---
+++ react/project1/src/route/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.css";
+
+function About(props){
+	console.log(props);
+	return (
+		<div className = "about__container">
+			<span>
+				"Give your entire soul and hardwork towars yourg goal";
+			</span>
+			<span> -- Brother </span>
+		</div>
+		);
+}
+
+export default About;
